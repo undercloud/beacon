3345a311086ec185cfeb94a4643c01f840d4eee4
undercloud <lodashes@gmail.com>
2017-09-13 15:31:47 +0400

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,10 @@
 <?php
+    // backward compatibility
+    if (!class_exists('\PHPUnit\Framework\TestCase', true)) {
+        class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+    } elseif (!class_exists('\PHPUnit_Framework_TestCase', true)) {
+        class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+    }
+
 	require_once __DIR__ . '/../src/Beacon/Beacon.php';
 ?>
